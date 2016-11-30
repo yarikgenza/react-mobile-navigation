96808134374ed088ac812921049f2046d7c6187b
Rostyslav Khanas <rtkhanas@gmail.com>
2016-11-30 21:40:26 +0900

    Fix example

--- packages/react-mobile-navigation-engine/examples/main/stack/components/pages/SettingsMainPageComponent.js
+++ packages/react-mobile-navigation-engine/examples/main/stack/components/pages/SettingsMainPageComponent.js
@@ -1,64 +1,63 @@
-import * as React from 'react';
+import React from 'react';
 import * as SettingsModeTypesEnum from '../../enum/settings-mode-types-enum';
 import {
-	DirectionEnum,
-	CustomPageBody,
-	PageWrapper,
+  DirectionEnum,
+  CustomPageBody,
+  PageWrapper,
 } from 'react-mobile-navigation-core';
 
 export class SettingsMainPageComponent extends React.Component {
 
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
+    this.connectedListText = this.connectedListText.bind(this);
+    this.connectedHelpText = this.connectedHelpText.bind(this);
+  }
 
-		this.connectedListText = this.connectedListText.bind(this);
-		this.connectedHelpText = this.connectedHelpText.bind(this);
-	}
+  connectedListText() {
+    this.props.pagingActions.openPage(
+      this.props.stackId,
+      SettingsModeTypesEnum.LICENSES,
+      DirectionEnum.HORIZONTAL
+    );
+  }
 
-	connectedListText() {
-		this.props.pagingActions.openPage(
-			this.props.stackId,
-			SettingsModeTypesEnum.LICENSES,
-			DirectionEnum.HORIZONTAL
-		);
-	}
+  connectedHelpText() {
+    this.props.pagingActions.openPage(
+      this.props.stackId,
+      SettingsModeTypesEnum.HELP,
+      DirectionEnum.HORIZONTAL
+    );
+  }
 
-	connectedHelpText() {
-		this.props.pagingActions.openPage(
-			this.props.stackId,
-			SettingsModeTypesEnum.HELP,
-			DirectionEnum.HORIZONTAL
-		);
-	}
-
-	render() {
-		const LIST_TEXT = 'LIST_TEXT';
-		const HELP_TEXT = 'HELP_TEXT';
-		return (
-			<PageWrapper>
-				<CustomPageBody zIndex={ this.props.pageState.zIndex } >
-					<div>
-						<div onClick={this.connectedListText} >
-							{LIST_TEXT}
-						</div>
-					</div>
-					<div>
-						<div onClick={this.connectedHelpText} >
-							{HELP_TEXT}
-						</div>
-					</div>
-				</CustomPageBody>
-			</PageWrapper>
-		);
-	}
+  render() {
+    const LIST_TEXT = 'LIST_TEXT';
+    const HELP_TEXT = 'HELP_TEXT';
+    return (
+      <PageWrapper>
+        <CustomPageBody zIndex={ this.props.pageState.zIndex } >
+          <div>
+            <div onClick={this.connectedListText} >
+              {LIST_TEXT}
+            </div>
+          </div>
+          <div>
+            <div onClick={this.connectedHelpText} >
+              {HELP_TEXT}
+            </div>
+          </div>
+        </CustomPageBody>
+      </PageWrapper>
+    );
+  }
 }
 
 SettingsMainPageComponent.defaultProps = {
-	pagingActions: undefined,
-	stackId: undefined,
+  pagingActions: undefined,
+  stackId: undefined,
 };
 
 SettingsMainPageComponent.propTypes = {
-	pagingActions: React.PropTypes.any,
-	stackId: React.PropTypes.any,
+  pagingActions: React.PropTypes.any,
+  stackId: React.PropTypes.any,
 };
