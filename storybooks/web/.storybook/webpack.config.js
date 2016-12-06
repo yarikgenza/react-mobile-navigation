f38670c6c5b220dc067581015e3220e7a5291bcd
Rostyslav Khanas <rtkhanas@gmail.com>
2016-12-06 21:15:41 +0900

    Ref

--- storybooks/web/.storybook/webpack.config.js
+++ storybooks/web/.storybook/webpack.config.js
@@ -3,12 +3,6 @@ const path = require('path');
 module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx'],
-    alias: {
-      'react-mobile-navigation-action-sheet': path.join(__dirname, '..', '..', '..', 'packages', 'react-mobile-navigation-action-sheet', 'src'),
-      'react-mobile-navigation-core': path.join(__dirname, '..', '..', '..', 'packages', 'react-mobile-navigation-core', 'src'),
-      'react-mobile-navigation-engine': path.join(__dirname, '..', '..', '..', 'packages', 'react-mobile-navigation-engine', 'src'),
-      'react-mobile-navigation-sheet': path.join(__dirname, '..', '..', '..', 'packages', 'react-mobile-navigation-sheet', 'src'),
-    },
   },
   module: {
     loaders: [
