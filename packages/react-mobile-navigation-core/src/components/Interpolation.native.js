ff942ffa61b140b7521f3ba75d6b69df55d83e5e
yarikgenza <gsmeter@gmail.com>
2017-08-17 19:20:38 +0300

    refactor

--- packages/react-mobile-navigation-core/src/components/Interpolation.native.js
+++ packages/react-mobile-navigation-core/src/components/Interpolation.native.js
@@ -1,29 +1,21 @@
 import isFunction from 'lodash/isFunction';
 import React, { Component } from 'react';
-import { Animated, Easing, TouchableWithoutFeedback, View } from 'react-native';
+import { Animated, Easing, View } from 'react-native';
 import PropTypes from 'prop-types';
 import { MODAL_MARGIN } from '../utils/style-api';
 
 const propTypes = {
   children: PropTypes.node,
   isVisible: PropTypes.bool.isRequired,
-  hideOnBack: PropTypes.bool,
-  hideOnBackdropPress: PropTypes.bool,
   pageHeight: PropTypes.number.isRequired,
   pageWidth: PropTypes.number.isRequired,
-  onClose: PropTypes.func,
   onPageOpenDone: PropTypes.func,
   onPageCloseDone: PropTypes.func,
-  onBackButtonPress: PropTypes.func,
 };
 
 const defaultProps = {
   children: undefined,
   isVisible: false,
-  hideOnBack: true,
-  hideOnBackdropPress: true,
-  onClose: () => null,
-  onBackButtonPress: () => null,
 };
 
 export class Interpolation extends Component {
@@ -35,8 +27,6 @@ export class Interpolation extends Component {
     };
     this.open = this.open.bind(this);
     this.close = this.close.bind(this);
-    this.closeOnBack = this.closeOnBack.bind(this);
-    this.closeOnBackdrop = this.closeOnBackdrop.bind(this);
   }
 
   componentWillMount() {
@@ -98,29 +88,12 @@ export class Interpolation extends Component {
     }
   }
 
-  closeOnBack() {
-    const { hideOnBack, onBackButtonPress } = this.props;
-    if (hideOnBack) {
-      this.close();
-    }
-    onBackButtonPress();
-  }
-
-  closeOnBackdrop() {
-    const { hideOnBackdropPress, onClose } = this.props;
-    if (hideOnBackdropPress) {
-      this.close();
-    }
-    onClose();
-  }
-
   render() {
     const { children, pageHeight, pageWidth } = this.props;
     const { isVisible } = this.state;
     const pageHeightNew = pageHeight;
     const pageWidthNew = pageWidth - (2 * MODAL_MARGIN);
     return isVisible ? (
-      <TouchableWithoutFeedback onPress={this.closeOnBackdrop}>
         <Animated.View
           removeClippedSubviews
           pointerEvents="box-none"
@@ -146,7 +119,6 @@ export class Interpolation extends Component {
             { pageHeight: pageHeightNew, pageWidth: pageWidthNew },
           ) : null}
         </Animated.View>
-      </TouchableWithoutFeedback>
     ) : null;
   }
 }
